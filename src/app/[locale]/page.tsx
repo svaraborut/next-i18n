11d035560bf2e433a15b4c0161959c76e5af988e
svaraborut <borutsvara@gmail.com>
2025-01-15 18:30:28 +0100

    cicd: automate watermark

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
 			<h1 className='text-xl font-bold'>Homepage</h1>
 			<h1>{t('title')}</h1>
 			<span className='font-mono'>
-				#{process.env.NEXT_PUBLIC_BRANCH}.{process.env.NEXT_PUBLIC_SHA}
+				{process.env.NEXT_PUBLIC_WATERMARK}
 			</span>
 		</div>
 	)
