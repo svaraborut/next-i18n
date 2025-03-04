fe6bab1083cd1cbd185e160610bc89b4ea5c66dc
svaraborut <borutsvara@gmail.com>
2025-03-04 15:02:33 +0100

    fix: font usage

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,16 +1,21 @@
 import { HTMLAttributes } from 'react'
 import { Geist, Geist_Mono } from 'next/font/google'
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
+const fontSans = Geist({
+	variable: '--font-sans',
 	subsets: ['latin']
 })
 
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
+const fontMono = Geist_Mono({
+	variable: '--font-mono',
 	subsets: ['latin']
 })
 
 export function Body({ className, ...props }: HTMLAttributes<HTMLBodyElement>) {
-	return <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} {...props} />
+	return (
+		<body
+			className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}
+			{...props}
+		/>
+	)
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,10 @@ export default {
 	content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['var(--font-sans)', 'sans-serif'],
+				mono: ['var(--font-mono)', 'monospace']
+			},
 			colors: {
 				background: 'var(--background)',
 				foreground: 'var(--foreground)',
